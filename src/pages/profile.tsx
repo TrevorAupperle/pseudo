c1ea9b8302d80677c5517bd4f079c2a727da1aa2
Addison Barrow <add9mob@gmail.com>
2024-02-04 06:25:03 -0600

    Added api utility to front-end to touch API endpoints

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React from "react";
 import { CodeSnippet } from "../components/code-snippet";
 import { PageLayout } from "../components/page-layout";
+import { getUserInfo, createUser } from '../utils/api.js';
 
 const Profile: NextPage = () => {
   const defaultPicture =
--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 //Routers import
 import usersRouter from './routers/usersRouter.js';
 import postsRouter from './routers/PostsRouter.js';
 
 const app = express();
-const PORT = '3000'
+const PORT = '3001'
 const uri = "mongodb+srv://pseudo:NPqik4rwuKwP5TuS@pseudo.xgrdlaa.mongodb.net/?retryWrites=true&w=majority";
 
 // Mongoose Connections
@@ -18,13 +19,17 @@ db.once("open", function () {
     console.log("Connected successfully");
 });
 
-// Parse Json Body 
+// Set up uses
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: false,
+}))
 app.use(bodyParser.json())
 
 // Attach routers to express
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+app.listen(PORT, () =>
+  console.log(`Example app listening on port ${PORT}!`),
 );
